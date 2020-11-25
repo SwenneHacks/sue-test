3810e0e807b0788cbb7d69421c8a96ad32f0cc3d
Swenne @Codam <swofferh@student.codam.nl>
2020-11-25 20:02:34 +0100

    added some explanation

--- test_lists.c
+++ test_lists.c
@@ -6,7 +6,7 @@
 /*   By: swofferh <swofferh@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/01/02 18:53:03 by swofferh       #+#    #+#                */
-/*   Updated: 2020/01/09 21:28:11 by swofferh      ########   odam.nl         */
+/*   Updated: 2020/11/25 20:00:55 by swofferh      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,7 +55,10 @@ int		test_lists(void)
 	t_list	*copy;
 	int		size;
 	char	*last;
-
+//
+	printf("\n\n THIS PART IS GOING TO SHOW YOU THE LINKED LISTS DANCE \n\n");	
+//
+	printf("> testing lstnew, add_back and add_front:\n");
 	lst = ft_lstnew(ft_strdup("susu"));
 	ft_lstadd_back(&lst, ft_lstnew(ft_strdup("sasa")));
 	ft_lstadd_front(&lst, ft_lstnew(ft_strdup("sisi")));
@@ -63,6 +66,7 @@ int		test_lists(void)
 	printf("%s\n", lst->next->content);
 	printf("%s\n\n", lst->next->next->content);
 //
+	printf("> testing lstiter:\n");
 	temp = lst;
 	ft_lstiter(lst, &up_f);
 	while (lst)
@@ -73,16 +77,19 @@ int		test_lists(void)
 	printf("\n");
 	lst = temp;
 //
+	printf("> testing lstmap:\n");
 	copy = ft_lstmap(lst, &low_f, &del);
 	printf("%s\n", copy->content);
 	printf("%s\n", copy->next->content);
-	printf("%s\n", copy->next->next->content);
+	printf("%s\n\n", copy->next->next->content);
 //
+	printf("> testing lstsize and last:\n");
 	size = ft_lstsize(lst);
 	last = ft_lstlast(lst)->content;
 	printf("size: %d\n", size);
 	printf("last: %s\n\n", last);
 //
+	printf("> testing a bit more:\n");
 	test = ft_lstnew(ft_strdup("test"));
 	ft_lstadd_front(&test, ft_lstnew(ft_strdup("this")));
 	ft_lstadd_front(&test, ft_lstnew(ft_strdup("then")));
@@ -90,10 +97,12 @@ int		test_lists(void)
 	printf("%s->", test->next->content);
 	printf("%s\n", test->content);
 //
+	printf("> before/after:\n");
 	printf("before: %d\n", ft_lstsize(test));
 	ft_lstclear(&test, &del);
 	printf("after: %d\n\n", ft_lstsize(test));
 //
+	printf("> testing lstdelone\n");
 	head = lst;
 	printf("%s->", head->next->next->content);
 	printf("%s->", head->next->content);
